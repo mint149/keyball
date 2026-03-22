ed74f0a0cb15bd58d694b91080a68d040a5f1939
Mint <skylove.exe@gmail.com>
2026-03-22 18:35:01 +0900

    keyball44ビルドエラー修正

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/pigeon/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/pigeon/config.h
@@ -1,6 +1,7 @@
 /*
 This is the c configuration file for the keymap
 
+Copyright 2022 @Yowkees
 Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
 
 This program is free software: you can redistribute it and/or modify
@@ -33,4 +34,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #define TAP_CODE_DELAY 5
+
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define HOLD_ON_OTHER_KEY_PRESS
+#define AUTO_MOUSE_DEFAULT_LAYER 5
+#define AUTO_MOUSE_TIME 1000
