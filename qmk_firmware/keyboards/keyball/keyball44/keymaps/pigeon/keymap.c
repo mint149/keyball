d9761e49c0209f9d82759b403699b609d12f6583
Mint <skylove.exe@gmail.com>
2026-03-26 07:09:38 +0900

    Keyball44に61に変更を反映

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/pigeon/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/pigeon/keymap.c
@@ -41,6 +41,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SQ_RBRC LSFT(KC_0)
 #define CR_LBRC LSFT(KC_LBRC)
 #define CR_RBRC LSFT(KC_RBRC)
+#define MACVOLD KC_KB_VOLUME_DOWN
+#define MACVOLU KC_KB_VOLUME_UP
 
 // Keyboard Quantizer B用キー設定
 // 左側のキーをRemap上で右側のキーとして配置する
@@ -86,7 +88,7 @@ enum custom_keycodes {
 
 bool imeOffOnly = false;
 bool imeOnOnly = false;
-bool isJisMode = true;
+bool isJisMode = false;
 bool isScrollInvert = false;
 
 bool isRecording = false;
@@ -116,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
 		KC_LGUI, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_ENT ,
 		KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                   KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_QUOT,
-						  KC_VOLD, KC_VOLU, KC_LGUI, IMEOFF , KC_SPC , KC_LCTL, IMEON  , /*    */ NOSPACE, NOSPACE, KC_CAPP
+						  MACVOLD, MACVOLU, KC_LGUI, IMEOFF , KC_SPC , KC_LCTL, IMEON  , /*    */ NOSPACE, NOSPACE, KC_CAPP
 	),
 
 	[_LOWER] = LAYOUT_universal(
